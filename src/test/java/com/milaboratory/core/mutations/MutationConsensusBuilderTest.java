068c47aa7eb4131abd17a01d0aa40e8f2d7462ba
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2016-02-02 17:38:34 +0300

    Test for MutationsAggregator.

--- src/test/java/com/milaboratory/core/mutations/MutationConsensusBuilderTest.java
+++ src/test/java/com/milaboratory/core/mutations/MutationConsensusBuilderTest.java
@@ -1,16 +1,33 @@
 package com.milaboratory.core.mutations;
 
+import com.milaboratory.core.alignment.Alignment;
+import com.milaboratory.core.mutations.generator.MutationModels;
+import com.milaboratory.core.mutations.generator.MutationsGenerator;
+import com.milaboratory.core.mutations.generator.NucleotideMutationModel;
+import com.milaboratory.core.sequence.NucleotideSequence;
+import com.milaboratory.test.TestUtil;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by poslavsky on 02/02/16.
- */
 public class MutationConsensusBuilderTest {
     @Test
     public void test1() throws Exception {
+        NucleotideMutationModel coreModel = MutationModels.getEmpiricalNucleotideMutationModel().multiplyProbabilities(3);
+        NucleotideMutationModel additionalModel = MutationModels.getEmpiricalNucleotideMutationModel().multiplyProbabilities(3);
+        NucleotideSequence coreSequence = TestUtil.randomSequence(NucleotideSequence.ALPHABET, 100, 200);
+        Mutations<NucleotideSequence> coreMutations = MutationsGenerator.generateMutations(coreSequence, coreModel);
+        NucleotideSequence mutatedCoreSequence = coreMutations.mutate(coreSequence);
 
+        MutationConsensusBuilder<NucleotideSequence> builder = new MutationConsensusBuilder<>(
+                NucleotideSequence.ALPHABET, coreSequence.size());
+        for (int i = 0; i < 1000; i++) {
+            Mutations<NucleotideSequence> additionalMutations =
+                    MutationsGenerator.generateMutations(mutatedCoreSequence, additionalModel);
+            Mutations<NucleotideSequence> totalMutations = coreMutations.combineWith(additionalMutations);
+            Alignment<NucleotideSequence> alignment = new Alignment<>(coreSequence, totalMutations, 0);
+            builder.aggregate(alignment, Weight.ONE);
+        }
 
+        AggregatedMutations<NucleotideSequence> result = builder.build();
+        //List<AggregatedMutations.Consensus<NucleotideSequence>> consensus = result.buildAlignments();
     }
 }
