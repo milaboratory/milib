37b55b97eeebd993d55e476cc0f7b99c6f5d0b7b
Alexander230 <fire_2005@mail.ru>
2017-10-18 13:09:00 +0300

    Implemented case sensitive alphabet and nucleotide sequence.

--- src/main/java/com/milaboratory/core/sequence/AbstractArraySequence.java
+++ src/main/java/com/milaboratory/core/sequence/AbstractArraySequence.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 /**
  * @author Dmitry Bolotin
  * @author Stanislav Poslavsky
+ * @author Aleksandr Popov
  */
 abstract class AbstractArraySequence<S extends AbstractArraySequence<S>> extends Sequence<S>
         implements java.io.Serializable {
@@ -31,6 +32,16 @@ abstract class AbstractArraySequence<S extends AbstractArraySequence<S>> extends
 
     protected AbstractArraySequence(char[] sequence) {
         this.data = dataFromChars(getAlphabet(), sequence);
+    }
+
+    protected AbstractArraySequence(String sequence, boolean caseSensitive) {
+        this.data = caseSensitive ? dataFromCharsCaseSensitive(getAlphabet(), sequence.toCharArray())
+                : dataFromChars(getAlphabet(), sequence.toCharArray());
+    }
+
+    protected AbstractArraySequence(char[] sequence, boolean caseSensitive) {
+        this.data = caseSensitive ? dataFromCharsCaseSensitive(getAlphabet(), sequence)
+                : dataFromChars(getAlphabet(), sequence);
     }
 
     protected AbstractArraySequence(byte[] data) {
@@ -80,6 +91,14 @@ abstract class AbstractArraySequence<S extends AbstractArraySequence<S>> extends
         byte[] data = new byte[chars.length];
         for (int i = 0; i < chars.length; ++i)
             if ((data[i] = alphabet.symbolToCode(chars[i])) == -1)
+                throw new IllegalArgumentException("Unknown symbol \"" + chars[i] + "\"");
+        return data;
+    }
+
+    protected static byte[] dataFromCharsCaseSensitive(Alphabet alphabet, char[] chars) {
+        byte[] data = new byte[chars.length];
+        for (int i = 0; i < chars.length; ++i)
+            if ((data[i] = alphabet.symbolToCodeCaseSensitive(chars[i])) == -1)
                 throw new IllegalArgumentException("Unknown symbol \"" + chars[i] + "\"");
         return data;
     }
