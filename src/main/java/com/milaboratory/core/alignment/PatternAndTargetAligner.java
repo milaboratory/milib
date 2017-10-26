e61fd104cef2a3834d4e1dba6a0887a33b26855c
Alexander230 <fire_2005@mail.ru>
2017-10-26 13:48:59 +0300

    Scoring and Aligner for pattern and target matching.

---
+++ src/main/java/com/milaboratory/core/alignment/PatternAndTargetAligner.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2017 MiLaboratory.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.milaboratory.core.alignment;
+
+import com.milaboratory.core.Range;
+import com.milaboratory.core.mutations.MutationsBuilder;
+import com.milaboratory.core.sequence.*;
+
+public final class PatternAndTargetAligner {
+    private PatternAndTargetAligner() {
+    }
+
+    /**
+     * Performs alignment of pattern and target with known position of pattern right side in the target and known
+     * maximum allowed number of indels.
+     *
+     * @param scoring scoring system for pattern and target alignment
+     * @param pattern pattern sequence, lowercase letters allow indels, uppercase letters don't allow
+     * @param target full target sequence with quality
+     * @param rightMatchPosition position of pattern right side in the target, inclusive
+     * @param maxIndels maximum allowed number of insertions and deletions
+     * @return alignment for pattern and target
+     */
+    public static Alignment<NucleotideSequenceCaseSensitive> alignLeftAdded(
+            PatternAndTargetAlignmentScoring scoring, NucleotideSequenceCaseSensitive pattern,
+            NSequenceWithQuality target, int rightMatchPosition, int maxIndels) {
+        int leftMatchPosition = Math.max(0, rightMatchPosition + 1 - pattern.size() - maxIndels);
+        if (leftMatchPosition < 0) leftMatchPosition = 0;
+        int patternSize = pattern.size();
+        int targetPartSize = rightMatchPosition - leftMatchPosition + 1;
+        NucleotideSequence targetSequence = target.getSequence();
+        SequenceQuality targetQuality = target.getQuality();
+
+        try {
+            MutationsBuilder<NucleotideSequenceCaseSensitive> builder = new MutationsBuilder<>(
+                    NucleotideSequenceCaseSensitive.ALPHABET);
+            CachedIntArray cachedArray = AlignmentCache.get();
+            int matrixSize1 = patternSize + 1;
+            int matrixSize2 = targetPartSize + 1;
+            BandedMatrix matrix = new BandedMatrix(cachedArray, matrixSize1, matrixSize2, maxIndels);
+            int i, j;
+
+            for (i = matrix.getRowFactor() - matrix.getColumnDelta(); i > 0; i--)
+                matrix.set(0, i, scoring.getGapPenalty(pattern, -1) * i);
+            for (i = matrix.getColumnDelta(); i > 0; i--)
+                matrix.set(i, 0, scoring.getGapPenalty(pattern, i - 1) * i);
+            matrix.set(0, 0, 0);
+
+            int match, delete, insert, to, gapPenalty;
+            for (i = 0; i < patternSize; i++) {
+                to = Math.min(i + matrix.getRowFactor() - matrix.getColumnDelta() + 1, targetPartSize);
+                gapPenalty = scoring.getGapPenalty(pattern, i);
+                for (j = Math.max(0, i - matrix.getColumnDelta()); j < to; j++) {
+                    match = matrix.get(i, j) +
+                            scoring.getScore(pattern.codeAt(patternSize - 1 - i),
+                                    targetSequence.codeAt(rightMatchPosition - j),
+                                    targetQuality.value(rightMatchPosition - j));
+                    delete = matrix.get(i, j + 1) + gapPenalty;
+                    insert = matrix.get(i + 1, j) + gapPenalty;
+                    matrix.set(i + 1, j + 1, Math.max(match, Math.max(delete, insert)));
+                }
+            }
+
+            // searching for maxScore
+            int maxI = -1, maxJ = -1;
+            int maxScore = Integer.MIN_VALUE;
+
+            if (maxScore < matrix.get(patternSize, targetPartSize)) {
+                maxScore = matrix.get(patternSize, targetPartSize);
+                maxI = patternSize;
+                maxJ = targetPartSize;
+            }
+
+            i = patternSize;
+            for (j = targetPartSize - maxIndels; j < matrixSize2; j++)
+                if (maxScore < matrix.get(i, j)) {
+                    maxScore = matrix.get(i, j);
+                    maxI = i;
+                    maxJ = j;
+                }
+
+            i = maxI - 1;
+            j = maxJ - 1;
+            gapPenalty = scoring.getGapPenalty(pattern, i);
+            byte c1, c2;
+            while (i >= 0 || j >= 0) {
+                if (i >= 0 && j >= 0 &&
+                        matrix.get(i + 1, j + 1) == matrix.get(i, j) +
+                                scoring.getScore(c1 = pattern.codeAt(patternSize - 1 - i),
+                                        c2 = targetSequence.codeAt(rightMatchPosition - j),
+                                        targetQuality.value(rightMatchPosition - j))) {
+                    if (c1 != c2)
+                        builder.appendSubstitution(patternSize - 1 - i, c1, c2);
+                    i--;
+                    j--;
+                    gapPenalty = scoring.getGapPenalty(pattern, i);
+                } else if (i >= 0 &&
+                        matrix.get(i + 1, j + 1) == matrix.get(i, j + 1) + gapPenalty) {
+                    builder.appendDeletion(patternSize - 1 - i,
+                            pattern.codeAt(patternSize - 1 - i));
+                    i--;
+                    gapPenalty = scoring.getGapPenalty(pattern, i);
+                } else if (j >= 0 &&
+                        matrix.get(i + 1, j + 1) == matrix.get(i + 1, j) + gapPenalty) {
+                    builder.appendInsertion(patternSize - 1 - i,
+                            targetSequence.codeAt(rightMatchPosition - j));
+                    j--;
+                } else
+                    throw new RuntimeException();
+            }
+
+            int patternStop = patternSize - maxI;
+            int targetStop = rightMatchPosition + 1 - maxJ;
+            int score = scoring.calculateAlignmentScore(maxScore, patternSize);
+            return new Alignment<>(pattern, builder.createAndDestroy(),
+                    new Range(patternStop, patternSize), new Range(targetStop, rightMatchPosition + 1), score);
+        } finally {
+            AlignmentCache.release();
+        }
+    }
+
+    /**
+     * Performs global alignment of pattern and part of the target.
+     *
+     * @param scoring scoring system for pattern and target alignment
+     * @param pattern pattern sequence, lowercase letters allow indels, uppercase letters don't allow
+     * @param targetPart part of the target: sequence with quality
+     * @return alignment for pattern and target part
+     */
+    public static Alignment<NucleotideSequenceCaseSensitive> alignGlobal(
+            PatternAndTargetAlignmentScoring scoring, NucleotideSequenceCaseSensitive pattern,
+            NSequenceWithQuality targetPart) {
+        int patternSize = pattern.size();
+        int targetPartSize = targetPart.size();
+        NucleotideSequence targetPartSequence = targetPart.getSequence();
+        SequenceQuality targetPartQuality = targetPart.getQuality();
+        int matrixSize1 = patternSize + 1;
+        int matrixSize2 = targetPartSize + 1;
+        int matrix[] = new int[matrixSize1 * matrixSize2];
+        int i1, i2, match, delete, insert;
+
+        for (int i = 0; i < matrixSize2; i++)
+            matrix[i] = scoring.getGapPenalty(pattern, -1) * i;
+        for (int j = 1; j < matrixSize1; j++)
+            matrix[matrixSize2 * j] = scoring.getGapPenalty(pattern, j - 1) * j;
+
+        for (i1 = 0; i1 < patternSize; i1++) {
+            int gapPenalty = scoring.getGapPenalty(pattern, i1);
+            for (i2 = 0; i2 < targetPartSize; i2++) {
+                match = matrix[i1 * matrixSize2 + i2] + scoring.getScore(
+                        pattern.codeAt(i1), targetPartSequence.codeAt(i2), targetPartQuality.value(i2));
+                delete = matrix[i1 * matrixSize2 + i2 + 1] + gapPenalty;
+                insert = matrix[(i1 + 1) * matrixSize2 + i2] + gapPenalty;
+                matrix[(i1 + 1) * matrixSize2 + i2 + 1] = Math.max(match, Math.max(delete, insert));
+            }
+        }
+
+        MutationsBuilder<NucleotideSequenceCaseSensitive> builder = new MutationsBuilder<>(
+                NucleotideSequenceCaseSensitive.ALPHABET, true);
+
+        i1 = patternSize - 1;
+        i2 = targetPartSize - 1;
+        int score = scoring.calculateAlignmentScore(matrix[(i1 + 1) * matrixSize2 + i2 + 1], patternSize);
+        int gapPenalty = scoring.getGapPenalty(pattern, i1);
+
+        while (i1 >= 0 || i2 >= 0) {
+            if (i1 >= 0 && i2 >= 0 &&
+                    matrix[(i1 + 1) * matrixSize2 + i2 + 1] == matrix[i1 * matrixSize2 + i2] + scoring.getScore(
+                            pattern.codeAt(i1), targetPartSequence.codeAt(i2), targetPartQuality.value(i2))) {
+                if (pattern.codeAt(i1) != targetPartSequence.codeAt(i2))
+                    builder.appendSubstitution(i1, pattern.codeAt(i1), pattern.codeAt(i2));
+                i1--;
+                i2--;
+                gapPenalty = scoring.getGapPenalty(pattern, i1);
+            } else if (i1 >= 0 &&
+                    matrix[(i1 + 1) * matrixSize2 + i2 + 1] == matrix[i1 * matrixSize2 + i2 + 1] + gapPenalty) {
+                builder.appendDeletion(i1, pattern.codeAt(i1));
+                i1--;
+                gapPenalty = scoring.getGapPenalty(pattern, i1);
+            } else if (i2 >= 0 &&
+                    matrix[(i1 + 1) * matrixSize2 + i2 + 1] == matrix[(i1 + 1) * matrixSize2 + i2] + gapPenalty) {
+                builder.appendInsertion(i1 + 1, targetPartSequence.codeAt(i2));
+                i2--;
+            } else
+                throw new RuntimeException();
+        }
+
+        return new Alignment<>(pattern, builder.createAndDestroy(),
+                new Range(0, patternSize), new Range(0, targetPartSize), score);
+    }
+}
---
+++ src/main/java/com/milaboratory/core/alignment/PatternAndTargetAlignmentScoring.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2017 MiLaboratory.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.milaboratory.core.alignment;
+
+import com.milaboratory.core.sequence.NucleotideAlphabetCaseSensitive;
+import com.milaboratory.core.sequence.NucleotideSequenceCaseSensitive;
+
+import java.io.ObjectStreamException;
+
+import static com.milaboratory.core.sequence.SequenceQuality.BAD_QUALITY_VALUE;
+import static com.milaboratory.core.sequence.SequenceQuality.GOOD_QUALITY_VALUE;
+
+/**
+ * Score system that uses penalty for gap, same as LinearGapAlignmentScoring, but with very high negative score
+ * for gaps and insertions near uppercase letters. It disables gaps and insertions near uppercase letters.
+ */
+public final class PatternAndTargetAlignmentScoring extends AbstractAlignmentScoring<NucleotideSequenceCaseSensitive>
+        implements java.io.Serializable  {
+    /**
+     * Penalty for gap near uppercase letter
+     */
+    private final int gapNearUppercasePenalty = -100000000;
+
+    private final int matchScore;
+    private final int mismatchScore;
+    private final int gapPenalty;
+    private final boolean usePatternLength;
+    private final byte goodQuality;
+    private final byte badQuality;
+    private final int maxQualityPenalty;
+
+    /**
+     * Creates new PatternAndTargetAlignmentScoring. Required for deserialization defaults.
+     */
+    @SuppressWarnings("unchecked")
+    private PatternAndTargetAlignmentScoring() {
+        super(NucleotideSequenceCaseSensitive.ALPHABET, new SubstitutionMatrix(Integer.MIN_VALUE, Integer.MIN_VALUE));
+        matchScore = Integer.MIN_VALUE;
+        mismatchScore = Integer.MIN_VALUE;
+        gapPenalty = Integer.MIN_VALUE;
+        usePatternLength = false;
+        goodQuality = GOOD_QUALITY_VALUE;
+        badQuality = BAD_QUALITY_VALUE;
+        maxQualityPenalty = 0;
+    }
+
+    /**
+     * Creates new PatternAndTargetAlignmentScoring.
+     *
+     * @param matchScore match score <= 0
+     * @param mismatchScore mismatch score < 0
+     * @param gapPenalty gap penalty < 0
+     * @param usePatternLength true if we shall consider pattern length in score calculation
+     * @param goodQuality this or better quality will not get score penalty
+     * @param badQuality this or worse quality will get maximal score penalty
+     * @param maxQualityPenalty score penalty value for badQuality or worse, <= 0
+     */
+    public PatternAndTargetAlignmentScoring(int matchScore, int mismatchScore, int gapPenalty, boolean usePatternLength,
+                                            byte goodQuality, byte badQuality, int maxQualityPenalty) {
+        super(NucleotideSequenceCaseSensitive.ALPHABET, new SubstitutionMatrix(matchScore, mismatchScore));
+        if ((matchScore > 0) || (mismatchScore >= 0) || (gapPenalty >= 0) || (maxQualityPenalty > 0))
+            throw new IllegalArgumentException();
+        this.matchScore = matchScore;
+        this.mismatchScore = mismatchScore;
+        this.gapPenalty = gapPenalty;
+        this.usePatternLength = usePatternLength;
+        this.goodQuality = goodQuality;
+        this.badQuality = badQuality;
+        this.maxQualityPenalty = maxQualityPenalty;
+    }
+
+    /**
+     * Get gap or insertion penalty by checking pattern subsequence: are there uppercase letters or not.
+     *
+     * @param pattern pattern case sensitive sequence
+     * @param x coordinate of the matrix cell to which we want to write result, in the pattern; it may be -1
+     * @return gap penalty
+     */
+    public int getGapPenalty(NucleotideSequenceCaseSensitive pattern, int x) {
+        NucleotideAlphabetCaseSensitive alphabet = NucleotideSequenceCaseSensitive.ALPHABET;
+        int left = Math.max(0, x - 1);
+        int right = Math.min(pattern.size() - 1, x + 1);
+        for (int i = left; i <= right; i++)
+            if (Character.isUpperCase(alphabet.codeToSymbol(pattern.codeAt(i))))
+                return gapNearUppercasePenalty;
+        return gapPenalty;
+    }
+
+    /**
+     * Get score for matched nucleotide using known target quality.
+     *
+     * @param from code of letter which is to be replaced
+     * @param to code of letter which is replacing
+     * @param quality quality for this matched nucleotide in target
+     * @return match score that includes correction based on target quality
+     */
+    public int getScore(byte from, byte to, byte quality) {
+        return getScore(from, to) + maxQualityPenalty * (Math.min(goodQuality, Math.max(badQuality, quality))
+                - badQuality) / Math.max(1, goodQuality - badQuality);
+    }
+
+    /**
+     * Update score for alignment using pattern length.
+     *
+     * @param rawAlignmentScore alignment score that doesn't include correction based on pattern length
+     * @param patternLength pattern length
+     * @return alignment score that includes correction based on pattern length
+     */
+    public int calculateAlignmentScore(int rawAlignmentScore, int patternLength) {
+        if (usePatternLength)
+            return (int)(rawAlignmentScore / (Math.log(patternLength + 1) / Math.log(2)));
+        else
+            return rawAlignmentScore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        PatternAndTargetAlignmentScoring that = (PatternAndTargetAlignmentScoring) o;
+
+        return matchScore == that.matchScore && mismatchScore == that.mismatchScore && gapPenalty == that.gapPenalty
+                && usePatternLength == that.usePatternLength && goodQuality == that.goodQuality
+                && badQuality == that.badQuality && maxQualityPenalty == that.maxQualityPenalty;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + matchScore;
+        result = 31 * result + mismatchScore;
+        result = 31 * result + gapPenalty;
+        result = 31 * result + (usePatternLength ? 1 : 0);
+        result = 31 * result + (int) goodQuality;
+        result = 31 * result + (int) badQuality;
+        result = 31 * result + maxQualityPenalty;
+        return result;
+    }
+
+    /* Internal methods for Java Serialization */
+
+    protected Object writeReplace() throws ObjectStreamException {
+        return new SerializationObject(matchScore, mismatchScore, gapPenalty, usePatternLength,
+                goodQuality, badQuality, maxQualityPenalty);
+    }
+
+    protected static class SerializationObject implements java.io.Serializable {
+        final int matchScore;
+        final int mismatchScore;
+        final int gapPenalty;
+        final boolean usePatternLength;
+        final byte goodQuality;
+        final byte badQuality;
+        final int maxQualityPenalty;
+
+        public SerializationObject() {
+            this(0, 0, 0, false, (byte)0, (byte)0, 0);
+        }
+
+        public SerializationObject(int matchScore, int mismatchScore, int gapPenalty, boolean usePatternLength,
+                                   byte goodQuality, byte badQuality, int maxQualityPenalty) {
+            this.matchScore = matchScore;
+            this.mismatchScore = mismatchScore;
+            this.gapPenalty = gapPenalty;
+            this.usePatternLength = usePatternLength;
+            this.goodQuality = goodQuality;
+            this.badQuality = badQuality;
+            this.maxQualityPenalty = maxQualityPenalty;
+        }
+
+        @SuppressWarnings("unchecked")
+        private Object readResolve()
+                throws ObjectStreamException {
+            return new PatternAndTargetAlignmentScoring(matchScore, mismatchScore, gapPenalty, usePatternLength,
+                    goodQuality, badQuality, maxQualityPenalty);
+        }
+    }
+}
