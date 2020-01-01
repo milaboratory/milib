b322ae03667dc1dae95b3465bc10389e6a251491
Bolotin Dmitriy <bolotin.dmitriy@gmail.com>
2020-01-01 18:39:28 +0300

    Final code for SampleBarcode generator.

--- src/test/java/com/milaboratory/core/tree/PrimerGenerator.java
+++ src/test/java/com/milaboratory/core/tree/PrimerGenerator.java
@@ -16,55 +16,84 @@
 package com.milaboratory.core.tree;
 
 import com.milaboratory.core.sequence.NucleotideSequence;
+import com.milaboratory.core.sequence.SequencesUtils;
 import com.milaboratory.test.TestUtil;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.Well19937c;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.Set;
 
 public class PrimerGenerator {
+
     @Test
     @Ignore
-    public void generate() {
+    public void generate() throws FileNotFoundException {
         Well19937c rg = new Well19937c();
-        // Set<NucleotideSequence> bestGen4 = null;
-        // for (int i = 0; i < 10; i++) {
-        //     Set<NucleotideSequence> gen4 = generateSequences(rg, new HashSet<>(),
-        //             TreeSearchParameters.FOUR_MISMATCHES_OR_INDELS, 6, 1_000_00, false);
-        //     if (bestGen4 == null || gen4.size() > bestGen4.size()) {
-        //         bestGen4 = gen4;
-        //         System.out.println("New size (4) = " + bestGen4.size());
-        //     }
-        // }
 
-        Set<NucleotideSequence> bestGen3 = null;
-        for (int i = 0; i < 100; i++) {
-            Set<NucleotideSequence> gen3 = generateSequences(rg, new HashSet<>(),
-                    TreeSearchParameters.THREE_MISMATCHES_OR_INDELS, 6, 1_000_00, false);
-            if (bestGen3 == null || gen3.size() > bestGen3.size()) {
-                bestGen3 = gen3;
-                System.out.println("New size (3) = " + bestGen3.size());
+        for (int length = 6; length <= 7; length++) {
+            for (int minDistance = 2; minDistance <= 4; minDistance++) {
+                for (int maxHomopolymer = 2; maxHomopolymer <= 5; maxHomopolymer++) {
+                    Set<NucleotideSequence> best = null;
+                    for (int i = 0; i < 100; i++) {
+                        TreeSearchParameters searchParameters;
+                        switch (minDistance) {
+                            case 2:
+                                searchParameters = TreeSearchParameters.TWO_MISMATCHES_OR_INDELS;
+                                break;
+                            case 3:
+                                searchParameters = TreeSearchParameters.THREE_MISMATCHES_OR_INDELS;
+                                break;
+                            case 4:
+                                searchParameters = TreeSearchParameters.FOUR_MISMATCHES_OR_INDELS;
+                                break;
+                            default:
+                                throw new IllegalArgumentException();
+                        }
+                        Set<NucleotideSequence> gen = generateSequences(rg,
+                                new HashSet<>(),
+                                searchParameters,
+                                length, maxHomopolymer, 1_000_00,
+                                false);
+                        if (best == null || gen.size() > best.size())
+                            best = gen;
+                    }
+                    System.out.println("Best ford length=" + length +
+                            " minDistance=" + minDistance +
+                            " maxHomo=" + maxHomopolymer +
+                            ": " + best.size());
+                    try (PrintStream ps = new PrintStream(
+                            "length" + length +
+                                    "_minDistance" + minDistance +
+                                    "_maxHomo" + maxHomopolymer + ".txt")) {
+                        for (NucleotideSequence seq : best)
+                            ps.println(seq);
+                    }
+                }
             }
         }
+
 
         // System.out.println("4");
         //
         // for (NucleotideSequence seq : bestGen4)
         //     System.out.println(seq);
 
-        System.out.println("3");
-
-        for (NucleotideSequence seq : bestGen3)
-            System.out.println(seq);
+        // System.out.println("3");
+        //
+        // for (NucleotideSequence seq : best)
+        //     System.out.println(seq);
     }
 
     public Set<NucleotideSequence> generateSequences(RandomGenerator rg,
                                                      Set<NucleotideSequence> exclude,
                                                      TreeSearchParameters treeSearchParameters,
                                                      int length,
+                                                     int maxHomopolymerLength,
                                                      long attempts,
                                                      boolean log) {
         SequenceTreeMap<NucleotideSequence, Boolean> tree = new SequenceTreeMap<>(NucleotideSequence.ALPHABET);
@@ -74,16 +103,7 @@ public class PrimerGenerator {
         Set<NucleotideSequence> generated = new HashSet<>();
         for (int i = 0; i < attempts; i++) {
             NucleotideSequence newSeq = TestUtil.randomSequence(NucleotideSequence.ALPHABET, rg, length, length);
-            int n = -1, l = 0, lm = 0;
-            for (int j = 0; j < length; j++) {
-                if(n != newSeq.codeAt(j)){
-                    n = newSeq.codeAt(j);
-                    l = 0;
-                }
-                ++l;
-                lm = Math.max(l, lm);
-            }
-            if(lm > 4)
+            if (SequencesUtils.findLongestHomopolymer(newSeq).length() > maxHomopolymerLength)
                 continue;
             Boolean searchResult = tree.getNeighborhoodIterator(newSeq, treeSearchParameters).next();
             if (searchResult == null) {
